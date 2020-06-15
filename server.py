91ebe010bf28e63f24d6e1281b5585c017a47a42
zoli4c <giangdeokinh@gmail.com>
2020-06-15 19:41:13 +0700

    Test subcriber

--- server.py
+++ server.py
@@ -2,19 +2,25 @@ import paho.mqtt.client as mqtt
 import time
 import json
 from random import *
+
+brokerIP = "52.187.144.179"
+
+#Callback function
 def on_connect(client, usrdata, flags, rc):
     if rc==0:
-        print("connect ok")
-client=mqtt.Client("python1")
-client.on_connect=on_connect
-client.connect("52.187.144.179",1883)
-client.loop_start()
-while 1:
-    #send msg
-    temperature=random()
-    humidity=random()
-    msg={ "device_id":"d5_1", "value":[str(temperature),str(humidity)]}
-    y=json.dumps(msg)
-    client.publish("home",y)
-    time.sleep(5)
-client.loop_stop()
+        print("Connect Successfull!")
+        # Subscribe topic "home/light"
+        client.Subscribe("home/light")
+
+def on_message(client, usrdata, msg):
+    print (str(msg.payload))
+
+#msg={ "device_id":"d5_1", "value":[str(temperature),str(humidity)]}
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(brokerIP,1883)
+
+client.loop_forever()
